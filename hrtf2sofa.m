4aa49f40d0d571ff98fba78928f5dd776a4b99a1
isaacengel <isakengel@gmail.com>
2021-02-24 15:56:02 +0100

    added 2 missing scripts

---
+++ hrtf2sofa.m
@@ -0,0 +1,50 @@
+function SOFA_obj = hrtf2sofa(h,fs,az,el,r)
+% Take HRIR data matrix and other paramters and make a SOFA object. Assume
+% "normal" conditions (free field, listener at the origin looking at the
+% front, etc).
+%
+% INPUT:
+%   h = HRIRs (time x dirs x ears)
+%   fs = sampling frequency in Hz
+%   az = azimuth in rad (dirs x 1)
+%   el = colatitude in rad (dirs x 1)
+%   r = head radius in m (def=0.085)
+%
+% OUTPUT:
+%   SOFA_obj = SOFA object, e.g. obtained from SOFAload()
+%
+% EXTERNAL DEPENDENCIES:
+%   SOFA API for Matlab (github.com/sofacoustics/API_MO)
+%
+% AUTHOR: Isaac Engel (isaac.engel@imperial.ac.uk)
+% February 2021
+
+if ~exist('r','var')
+    r = 0.085;
+end
+
+SOFA_obj.Data.IR = permute(h,[2,3,1]); % dirs x ears x time
+SOFA_obj.Data.SamplingRate = fs;
+SOFA_obj.Data.SamplingRate_Units = 'hertz';
+SOFA_obj.SourcePosition = [az(:)*180/pi,90-el(:)*180/pi,ones(size(az(:)))]; % HRTF directions
+SOFA_obj.SourcePosition_Type = 'spherical';
+SOFA_obj.SourcePosition_Units = 'degree,degree,metre';
+SOFA_obj.ListenerPosition = [0 0 0]; % listener at the origin
+SOFA_obj.ListenerPosition_Type = 'cartesian';
+SOFA_obj.ListenerPosition_Units = 'metre';
+SOFA_obj.ListenerView = [1 0 0]; % listener looking front
+SOFA_obj.ListenerView_Type = 'cartesian';
+SOFA_obj.ListenerView_Units = 'metre';
+SOFA_obj.ReceiverPosition = [0 r 0; 0 -r 0];
+SOFA_obj.ReceiverPosition_Type = 'cartesian';
+SOFA_obj.ReceiverPosition_Units = 'metre';
+SOFA_obj.EmitterPosition = [0,0,0]; % not used; this is the default value
+SOFA_obj.EmitterPosition_Type = 'cartesian';
+SOFA_obj.EmitterPosition_Units = 'metre';
+SOFA_obj.API.M = size(SOFA_obj.Data.IR,1);
+SOFA_obj.API.R = size(SOFA_obj.Data.IR,2);
+SOFA_obj.API.N = size(SOFA_obj.Data.IR,3);
+SOFA_obj.API.Dimensions.Data.IR = 'MRN'; % time-measurements-receivers
+SOFA_obj.GLOBAL_SOFAConventions = 'SimpleFreeFieldHRIR';
+SOFA_obj.GLOBAL_RoomType = 'free field';
+SOFA_obj.GLOBAL_Comment = 'Generated with ''makesofa()'' from an HRIR';
---
+++ sofa2hrtf.m
@@ -0,0 +1,47 @@
+function [h,fs,az,el,r] = sofa2hrtf(SOFA_obj)
+% Return HRIR data matrix and other paramters from a SOFA object. Assume
+% "normal" conditions (free field, listener at the origin looking at the
+% front, etc).
+%
+% INPUT:
+%   SOFA_obj = SOFA object, e.g. obtained from SOFAload()
+%
+% OUTPUT:
+%   h = HRIRs (time x dirs x ears)
+%   fs = sampling frequency in Hz
+%   az = azimuth in rad (dirs x 1)
+%   el = colatitude in rad (dirs x 1)
+%   r = head radius in m (def=0.085)
+%
+% EXTERNAL DEPENDENCIES:
+%   SOFA API for Matlab (github.com/sofacoustics/API_MO)
+%
+% AUTHOR: Isaac Engel (isaac.engel@imperial.ac.uk)
+% February 2021
+
+%% Check struct fields
+if ~strcmp(SOFA_obj.GLOBAL_SOFAConventions,'SimpleFreeFieldHRIR')
+    error('SOFA convention must be SimpleFreeFieldHRIR')
+end
+if ~strcmp(SOFA_obj.SourcePosition_Type,'spherical')
+    error('Source position must be in spherical coordinates')
+end
+if length(SOFA_obj.API.Dimensions.Data.IR)~=3
+    error('IR must have 3 dimensions (direction, ear, time)')
+end
+
+%% Assign data to output
+h = SOFA_obj.Data.IR;
+fs = SOFA_obj.Data.SamplingRate;
+az = SOFA_obj.SourcePosition(:,1)*pi/180; % azimuth in rad
+el = pi/2-SOFA_obj.SourcePosition(:,2)*pi/180; % colatitude in rad
+r = 0.0875; % using this as default for now
+
+%% Rearrange IR dimensions
+dimIn = SOFA_obj.API.Dimensions.Data.IR; % dimensions, e.g. 'MRN'
+dimOut = 'NMR'; % this is the order we want for the output
+dimIndx = nan(1,3);
+for i=1:3 % this is the order used in the output
+    dimIndx(i) = find(dimIn==dimOut(i));
+end
+h = permute(h,dimIndx);
